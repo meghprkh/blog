7a5c6c3d88bcf3c387d85c2e4138e1afe9dac409
Megh Parikh <meghprkh@gmail.com>
2023-02-12 02:01:37 -0500

    Add old category stuff

---
+++ unused/categories_eleventy.js
@@ -0,0 +1,21 @@
+function asArray(stringOrArray) {
+	if (stringOrArray === undefined) return [];
+	return Array.isArray(stringOrArray) ? stringOrArray : [stringOrArray];
+}
+
+eleventyConfig.addCollection("categories", function (collectionApi) {
+	let categorySet = new Set();
+	collectionApi.getAll().forEach((item) => {
+		asArray(item.data.categories).forEach((tag) => categorySet.add(tag));
+	});
+	return Array.from(categorySet);
+});
+
+eleventyConfig.addFilter(
+	"filterCategory",
+	function filterCategory(posts, category) {
+		return posts.filter(
+			(p) => asArray(p.data.categories).indexOf(category) != -1
+		);
+	}
+);
